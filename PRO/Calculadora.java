e6a52ab4a6394e1d4d6f3bb1e60a179b4c32ef10
Jose Luis Luzuriaga Alanis <119702567+JoseLuisADA@users.noreply.github.com>
2022-12-14 14:27:15 +0100

    Update Calculadora.java

--- PRO/Calculadora.java
+++ PRO/Calculadora.java
@@ -6,89 +6,128 @@ public class Calculadora {
 
         //VARIABLES DEL JUEGO
 
-        Scanner consola = new Scanner(System.in);
-        
+        int[][] numerosPartida = new int[3][3];
+        int ejeY = 0;
+        int ejeX = 0;
+
         boolean vidaJugador1 = true;
         boolean vidaJugador2 = true;
         
-        int numeroJugador1;
-        int numeroJugador2;
-
-        int total = 0;
+        int totalCalculadora = 0;
 
         //INICIO DEL JUEGO
 
-        System.out.println(total);
+        System.out.println(
+            "-----------------------------\n" + 
+            "TOTAL CALCULADORA:\n" +
+            "-----------------------------\n" +  
+            totalCalculadora +
+            "\n-----------------------------");
         
         do{
 
-            //TURNO JUGADOR 1 Y COMPROBACIÓN DE LA JUGADA
-            total += turnoJugador1();
-            
-            System.out.println(total);
+            //TURNO JUGADOR 1 Y COMPROBACIÓN DE LA JUGADA 
+            totalCalculadora += turnoJugador1();
 
-            if(total >= 31){
-                vidaJugador1 = false;
-                break;
-            }
+            //MOSTRAR EL TOTAL DE LA CALCULADORA Y COMPROBAR SI SE HA PASADO EL JUGADOR
+            vidaJugador1 = comprobarTotalCalculadora(totalCalculadora);
 
             //TURNO JUGADOR 2 Y COMPROBACIÓN DE LA JUGADA
-            total += turnoJugador2();
+            
+            totalCalculadora += turnoJugador2();
 
-            if(total >= 31){
-                vidaJugador2 = false;
-                break;
-            }
+            //MOSTRAR EL TOTAL DE LA CALCULADORA Y COMPROBAR SI SE HA PASADO EL JUGADOR
+            vidaJugador2 = comprobarTotalCalculadora(totalCalculadora);
 
         } while(vidaJugador1 == true && vidaJugador2 == true);
         
-        //COMPROBACIÓN GANADOR
+        //ANUNCIAR GANADOR
+        anunciarGanador(vidaJugador1, vidaJugador2);
 
+    }
+
+    //ÓRGANOS DEL JUEGO xD
+
+    public static int capturarNumJugador(){
+        Scanner consola = new Scanner(System.in);
+        int numeroJugador = consola.nextInt();
+        return numeroJugador;
+    }
+
+    public static boolean comprobarTotalCalculadora(int total){  
+
+        System.out.println(
+        "-----------------------------\n" + 
+        "TOTAL CALCULADORA:\n" +
+        "-----------------------------\n" +  
+        total +
+        "\n-----------------------------");
+        
+        if(total >= 31){
+            return false;
+        } else {
+            return true;
+        }  
+    }
+
+    public static boolean comprobarJugada(int numeroJugador){
+       
+        boolean sigueLasReglas;
+
+        if((numeroJugador > 9 || numeroJugador <= 0)){
+            System.out.println("Ingrese un número acorde a las reglas");
+             sigueLasReglas = false;
+        } else {
+            sigueLasReglas = true;
+        }
+
+        return sigueLasReglas;
+    }
+
+    public static int turnoJugador1(){
+        boolean sigueLasReglas;
+        int numeroJugador1;
+        
+        do{
+
+            System.out.println("Turno Jugador 1:");
+            numeroJugador1 = capturarNumJugador();
+
+            sigueLasReglas = comprobarJugada(numeroJugador1);
+
+        }
+        while(sigueLasReglas == false);
+
+        return numeroJugador1;
+    }
+
+    public static int turnoJugador2(){
+        
+        boolean sigueLasReglas;
+        int numeroJugador2;
+        
+        do{
+
+            System.out.println("Turno Jugador 2:");
+            numeroJugador2 = capturarNumJugador();
+
+            sigueLasReglas = comprobarJugada(numeroJugador2);
+            
+        }
+        while(sigueLasReglas == false);
+
+        return numeroJugador2;
+    }
+    
+    public static void anunciarGanador(boolean vidaJugador1, boolean vidaJugador2){
         if(vidaJugador1 == true){
             System.out.println("Ganador Jugador 1");
         } else if(vidaJugador2 == true){
             System.out.println("Ganador Jugador 2");
         }
-        
-
     }
 
-    public static int comprobarJugada(int numeroJugador){
-        
-        if(numeroJugador > 9 || numeroJugador <= 0){
-            System.out.println("Ingrese un número acorde a las reglas");
-            numeroJugador = 0;
-            turnoJugador1();
-        }
-
-        return numeroJugador;
+    public static void añadirNumeroEnArray(int[][] numerosPartida, int numeroJugador, int ejeY, int ejeX){
+        numerosPartida[ejeY][ejeX] = numeroJugador;
     }
-
-    public static int turnoJugador1(){
-        
-        Scanner consola = new Scanner(System.in);
-        
-        System.out.println("Turno Jugador 1:");
-        int numeroJugador1 = consola.nextInt();
-        
-        comprobarJugada(numeroJugador1);
-
-        return numeroJugador1;
-       
-    }
-
-    public static int turnoJugador2(){
-        
-        Scanner consola = new Scanner(System.in);
-
-        System.out.println("Turno Jugador 2:");
-        int numeroJugador2 = consola.nextInt();
-        
-        comprobarJugada(numeroJugador2);
-
-        return numeroJugador2;
-    }
-
-    
-
 }
