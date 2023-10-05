22d8238cb743a117290d57a3b0fc2c7e9fdbe6e4
Jose Luis Luzuriaga Alanis <joseluis.luzuriagaalanis.web@adaits.es>
2023-10-05 14:31:47 +0200

    SEPARACION DE APARTADOS

--- segundo/entornoServidor/NodeJS/05-nodejs-object/basico.js
+++ segundo/entornoServidor/NodeJS/05-nodejs-object/basico.js
@@ -1,13 +1,19 @@
+//APARTADO 1
+
 function getVoteCount(votes) {
 	
     return votes.upvotes - votes.downvotes;
 
 }
 
+//APARTADO 2
+
 function volumeOfBox(sizes) {
     return sizes.length * sizes.width * sizes.height;
 	
 }
+
+//APARTADO 3
 
 
 const user = { name: "John", email: "john@example.com",city: "Phoenix", state: "AZ", country: "USA"}
--- segundo/entornoServidor/NodeJS/05-nodejs-object/facil.js
+++ segundo/entornoServidor/NodeJS/05-nodejs-object/facil.js
@@ -1,3 +1,17 @@
+//APARTADO 1
+
 function toArray(obj) {
 	return Object.entries(obj);
 }
+
+//APARTADO 2
+
+function keysAndValues(obj) {
+	
+}
+
+
+let obj = {nombre: "Jose", apellidos: "Luis"}
+let obj2 = {nombre: "Luzuriaga", apellidos: "Alanis"}
+
+console.log(Object.keys(obj).concat(Object.values(obj2)))
