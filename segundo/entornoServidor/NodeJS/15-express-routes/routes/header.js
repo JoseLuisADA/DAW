e90098624f86e38a40dcf450988ee39dca79b35a
Jose Luis Luzuriaga Alanis <joseluis.luzuriagaalanis.web@adaits.es>
2023-11-08 14:28:53 +0100

    AVANCES 15-EXPRESS-ROUTER

--- segundo/entornoServidor/NodeJS/15-express-routes/routes/header.js
+++ segundo/entornoServidor/NodeJS/15-express-routes/routes/header.js
@@ -3,12 +3,23 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
+  let message;
   const { token } = req.query;
   if (token) {
+    message = {
+      code: 200,
+      asunto: 'Authorized',
+      message: 'Identificado correctamente',
+    };
+    res.status(200).send(message);
     console.log(token);
   } else {
-    const object = {};
-    res.status(400).send('token is required');
+    message = {
+      code: 401,
+      error: 'Unauthorized',
+      message: 'Error: Set a token to login',
+    };
+    res.status(401).send(message);
   }
 });
 
